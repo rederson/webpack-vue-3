4a27d6f37aeda16cf143738a72a5cc3c8706956c
rederson <r.ataliba@hotmail.com>
2023-05-02 22:51:33 -0300

    Primeiro commit

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path =  require('path');
+const {VueLoaderPlugin} = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:'./src/main.js'
+
+}
